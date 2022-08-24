8209b7f99445fd6c51ba92d055ccdacd5dfed39e
leeyoonseo <okayoon.lee@gmail.com>
2022-08-24 20:44:43 +0900

    클래스 (implements, abstract, private, protected, public..)

--- first.ts
+++ first.ts
@@ -42,3 +42,103 @@ const aaaa: A = { a: 3, b: 5, c: 5, d: 123};
 type B = 'Human' | 'Mammal' | 'Animal'; // - interface는 | (또는)이 안된다. -> 인터페이스에서는 &이 안된다.
 type AA = {[key in B]: number}; // key가 B의 3개 문자열 중에 하나
 const aaaaa: AA = { Human: 123, Mammal: 5, Animal: 7};
+
+// 클래스일 경우 
+class C  {
+  a: string;
+  b: number;
+  constructor(a: string, b: number = 123) { // 기본 값이 있을때는 ?를 안 붙여도 됨
+    this.a = '123';
+    this.b = 123;
+  }
+
+  method() {}
+}
+
+const c = new C('123', 456);
+
+// class 이름은 그 자체로 타입이 될 수 있다.
+// new C를 타입으로 가르키는 타입
+type CC = C;
+const cc: C = new C('123'); 
+// class 자체를 가르키는 타입
+const e: typeof C = C; // C는 클래스 자체를 가르키는 타입은 [typeof C], 클래스 이름은 인스턴스를 가르킨다. [C]
+
+class D  {
+  // #이 붙으면 private인데, typescript의 private과는 다르다.
+  // typescript (이것을 쓰는 것을 추천) - protected등 다른 키워드들도 있기 때문에 정교하게 사용 가능함
+  // 대신 typescript에 private는 실제 코드에서는 public으로 바뀐다.
+  // 하지만 실제 개발 시 typqscript에서 private를 public으로 사용할 수 없기에 (에러) 개발 시 문제가 안된다고본다.
+  private a: string = '123';
+  // javascript
+  #b: number = 123; 
+
+  method() {
+    // private일 경우 자기 class 내부에서만 쓸 수 있다.
+    console.log(this.a, this.#b); 
+  }
+}
+
+interface E {
+  readonly a: string;
+  b: string;
+  // private a: string;
+  // protected  b: string;
+  // public c: string;
+}
+
+// implements - class는 interface를 구현(implements)할 수 있다.
+// interface가 있으면 class는 interface를 따라야한다. (즉 F가 E를 따라야한다.)
+// interface와 implements는 js가 되면 사라진다. -> 경고를 통해 class의 모양을 interface로 통제할 수 있다.
+class F implements E { 
+  // 내부에서 키워드 사용가능하므로 굳이 implements 사용안한다고 함(=이건 개취)
+  // 객체지향을 중시할 경우 사용 
+  // - 추상에 의존하고 구현에 의존하지 말라를 원칙적으로 지키기 위해서 사용해도됨
+  // - 추상(interface)과 구현(class)
+
+  a: string = '123';
+  b: string = 'word';
+
+  // 기본은 public이라서 따로 안써도 됨, 인스턴스에서 일반적으로 접근할 수 있는 것(내외부 사용 가능)
+  // public c: string = 'wow';
+  // 일반적으로 접근 할 수 없음. 클래스 안에서만 사용해야함(외부에서 사용할 수 없다.)
+  // private a: string = '123';
+  // class 안에서 사용할 수 있는데 클래스 밖에서 사용할 수 없다. 하지만 상속받고 나서는 사용 가능
+  // protected b: string = 'word';
+
+  // 키워드 섞어서 복합적으로 사용 가능
+  // private readonly a: string = '123';
+}
+
+class G extends F { 
+  // F를 상속받고 있기 때문에 F의 protected 프로퍼티 사용 가능
+  method() {
+    // console.log(this.a); // private여서 사용 불가
+    console.log(this.b);
+    // console.log(this.c);
+  }
+}
+
+// new F().a;
+// new F().b;
+
+// 추상화
+// - class도 추상 클래스가 가능하다
+abstract class H {
+  // 추상 클래스가 좋은 점은 추상 메서드 사용이 가능하다.
+  // 클래스를 추상적으로 대충 모양만 만들어두고 이것을 extends할때 추상 클래스 사용
+  // 따라서 추상 클래스가 있으므로 interface(추상) implements하는 것에 대한 고민은 필요할 듯!
+  // 추상 메서드는 반드시 상속받았을때 구현해야한다.
+  abstract method(): void;
+
+  // 추상 메서드 외 실제 메서드를 가지고 있을 수도 있다.
+  method2() {
+    return '3';
+  }
+}
+
+class I extends H {
+  method() {
+    console.log('추상 메서드는 반드시 상속받았을때 구현해야한다.');
+  }
+}
