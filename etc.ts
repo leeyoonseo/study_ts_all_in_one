106ceda1a99ad56650653d8730e6ec40a3707117
leeyoonseo <okayoon.lee@gmail.com>
2022-08-31 21:23:34 +0900

    오버로딩, error처리, interface와 class 차이 등등

--- etc.ts
+++ etc.ts
@@ -84,3 +84,52 @@ class CAdd {
 
 const cadd = new CAdd().add(1, 2);
 const cadd2 = new CAdd().add('1', '2');
+
+// 타입스크립트 조심해야하는 점!!!
+interface Axios {
+  get(): void;
+}
+
+// interface CustomError extends Error { // Error에서 확장
+class CustomError extends Error { // Error에서 확장
+  // interface와 class 차이
+  // 선택을 어떻게 해야하는가?
+  // interface일때 (type도) js에서 사라짐. 그럴경우 try, catch에서 instanceof 같은 것을 사용할 수 없다.
+  // 만약 instanceof 같이 js에서 사용해야할 경우 class를 통해 타입 선언을 해줘야한다.
+
+  // 자바스크립트 기본 Error 타입 항목 
+  // name: string;
+  // messgae: string;
+  // stack?: string;
+  
+  response?: { // axios를 쓰면 axios에서 추가한다함
+    data: any;
+  }
+}
+
+declare const axios: Axios;
+
+(async () => {
+  try {
+    await axios.get();
+  // } catch (error: unknown) { // 무조건 unknown이고 unknow은 무조건 as로 타입 정의
+  } catch (error) { // if문으로 타입가드가 있으므로 unknown 타입 선언 안해도된다.
+    // console.error((error as CustomError).response?.data); // 인터페이스에서 옵셔널일 경우를 고려해서 ?.를 추가해야한다.
+    // error.response // 위에서 as CustomError를 정의해줬지만 타입스크립트는 1회성 이므로 하위에서 다시 선언해야하는 경우가 생긴다.
+    // 그러므로 타입을 강제로 바꾼것을 변수로 만들어서 타입을 저장하는 것이 좋다.
+    // const customError = error as CustomError; // as를 써야할때 unknown일때 어쩔수 없이 써야한다. 
+    // console.error(customError.response?.data);
+    // customError.response?.data;
+    
+    // if문으로 error이 있는지 확인해야 에러가 안남.
+    if (error instanceof CustomError) { // 따라서 catch문에서도 최대한 as를 안쓰는게 가장 베스트다.
+      // if문 === 타입가드, 이것으로 인해 error는 if문 안에서는 customError로 타입 추론된다.
+      // interface 자체를 타입가드에서 사용할 수 없다.
+      console.error(error.response?.data);
+      error.response?.data;
+    }
+  }
+})();
+
+// any 타입 검사 포기
+// unknown 타입 붙이든 안전하게 써라~ (타입 검사 포기하지는 않음)
