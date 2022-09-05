891503bcebfef70ff3154fab600cccff2afa76d2
leeyoonseo <okayoon.lee@gmail.com>
2022-09-05 14:32:29 +0900

    Pick 타입 분석

--- utilititypes.ts
+++ utilititypes.ts
@@ -31,3 +31,29 @@ const newCho: UPartial<UProfile> = {
   name: 'cho',
   age: 30,
 };
+
+// 하지만 Partial은 좋다고 할 수 없다.
+// Pick은 원하는 것만 가져올 수 있다.
+// const choPick: Pick<UProfile, 'name' | 'age'> = {
+//   name: 'cho',
+//   age: 30,
+// };
+
+// Pick 만들어보기
+// 제네릭을 쓰면 제네릭에 대한 제한조건을 먼저 쓰면 좋다.
+// UProfile 과 'name', 'age' 둘 사이의 관계가 있으므로 extends keyof T 를 써줘야한다.
+type UPick<T, S extends keyof T> = {
+  [key in S]: T[key];
+};
+
+const choPick: UPick<UProfile, 'name' | 'age'> = {
+  name: 'cho',
+  age: 30,
+};
+
+// Omit은 하나만 제외할 때 사용
+const choOmit: Omit<UProfile, 'married'> = {
+  name: 'cho',
+  age: 30,
+};
+
