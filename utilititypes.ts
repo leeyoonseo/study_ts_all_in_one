c837531326b1fec907f9a5759f8cbc8001eca951
leeyoonseo <okayoon.lee@gmail.com>
2022-09-05 14:50:31 +0900

    omit, exclude, extract 분석

--- utilititypes.ts
+++ utilititypes.ts
@@ -52,8 +52,33 @@ const choPick: UPick<UProfile, 'name' | 'age'> = {
 };
 
 // Omit은 하나만 제외할 때 사용
-const choOmit: Omit<UProfile, 'married'> = {
+// const choOmit: Omit<UProfile, 'married'> = {
+//   name: 'cho',
+//   age: 30,
+// };
+
+// Omit 만들어보기 -> exclude를 먼저 알아야함. -> 여러개의 utility type들을 쓰고 있음.
+// Exclude는 제외하는 것
+type UAnimal = 'Cat' | 'Dog' | 'Human';
+type Mammal = Exclude<UAnimal, 'Human'>; // Human이 제외됨
+type UExclude = Exclude<keyof UProfile, 'married'>; // married가 제외됨
+const choPickandExclude: Pick<UProfile, Exclude<keyof UProfile, 'married'>> = {
   name: 'cho',
   age: 30,
 };
 
+// Omit 만들어보기 -> pick, exclude를 조합해서 만들어야함.
+type UOmit<T, S extends keyof T> = Pick<T, Exclude<keyof T, S>>;
+const choOmit: UOmit<UProfile, 'married'> = {
+  name: 'cho',
+  age: 30,
+};
+
+// Exclude, Extract 만들어보기
+// 비교 (서로 반대임) - 타입에 3항 연산도 가능함..
+// type Exclude<T, U> = T extends U ? never : T; // 부분 집합이면 never (없앤다.) = 맞으면 없앰
+// type Extract<T, U> = T extends U ? T: never; // 부분 집합이면 남겨라 = 맞으면 추출
+
+
+// Extract 추출하는것 
+type Human = Extract<UAnimal, 'Human'>;
