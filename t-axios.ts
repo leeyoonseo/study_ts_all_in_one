0bf20d44bb182ab5d7324156c629558879456b1c
leeyoonseo <okayoon.lee@gmail.com>
2022-09-06 17:59:15 +0900

    axios 타입 만들어보기

--- t-axios.ts
+++ t-axios.ts
@@ -1,17 +1,69 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
-
-// api test: https://jsonplaceholder.typicode.com/
-
 // 브라우저 fetch
 // 노드 fetch
 // axios = fetch (사실 상 XMLHttpRequest) + 여러기능
+
+// api test: https://jsonplaceholder.typicode.com/
 
 interface Post { userId: number, id: number, title: string, body: string }
 interface Created { }
 interface Data { title: string, body: string, userId: number }
 
+// 그냥 any 넣는것 보다 제네릭으로 기본값 any로해서 넣는것이 더 좋다.
+// 타인이 제네릭을 사용할 수도 있기 때문에 (확장)
+interface Config<D = any> {
+  method?: 'post' | 'get' | 'put' | 'patch' | 'delete' | 'head' | 'options';
+  url?: string;
+  data?: D;
+}
+
+// axios 타입 만들어보기
+// await 붙는 것들은 return Promise (Promise는 타입스크립트에서 기본적으로 제공함)
+interface newAxios {
+  // 제네릭은 <T, AxiosResponse<T>> 변수 선언하여 써야함
+  // 제네릭을 안쓰고 싶은 사람을 위해서 T = any도 그냥 any 해줌(기본 값을 넣어줌)
+  get: <T = any, R = AxiosResponse<T>>(url: string) => Promise<R>,
+  // 필수 형식 매개 변수는 선택적 형식 매개 변수 다음에 올 수 없습니다.ts(2706)
+  // R = 값, 은 이미 기본 값이 있어서 선택으로 취급되기때문에 선택 뒤에 필수가 올 수 없다.
+  // 따라서 뒤에 Data에 대한 D도 기본 값을 넣어 선택으로 바꿔줘야함 (그래서 라이브러리에 D = any 이렇게 기본 값을 추가한 것에 대한 이유들임)
+  post: <T = any, R = AxiosResponse<T>, D = any>(url: string, data: D) => Promise<R>,
+  isAxiosError: (error: unknown) => error is AxiosError,
+  (config: Config): void,
+  (url: string, config: Config): void,
+}
+
+// 타입 만들어보기  
+const newAxios: newAxios = axios;
+
 (async () => {
   try {
+    // 타입 만들어보기
+    const newResponse = await newAxios.get<Post, AxiosResponse<Post>>('https://jsonplaceholder.typicode.com/posts/1');
+    const newResponse2 = await newAxios.post<Created, AxiosResponse<Created>, Data>('https://jsonplaceholder.typicode.com/posts', {
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    });
+    const newResponse3 = await newAxios({
+      method: 'post',
+      url: 'https://jsonplaceholder.typicode.com/posts',
+      data: {
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      }
+    });
+
+    const newResponse4 = await newAxios('https://jsonplaceholder.typicode.com/posts', {
+      method: 'post',
+      data: {
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      }
+    });
+    // // 타입 만들어보기
+
     // get, put, post 같은건 class Axios에 선언되어 있음.
     // get<T = any, R = AxiosResponse<T>, D = any>(url: string, config?: AxiosRequestConfig<D>): Promise<R>;
     // 결과 값 Promise<AxiosResponse<T>>;, T는 AxiosResponse의 data: T임
@@ -84,6 +136,11 @@ interface Data { title: string, body: string, userId: number }
       // console.error((error as AxiosError<{ message: string }>).response?.data.message); // as의 위치, 선언하는 interface만 다를뿐 아래와 동일함
       console.error((error.response as AxiosResponse<{ message: string }>)?.data.message);
     }
+
+    // 타입 만들어보자
+    if (newAxios.isAxiosError(error)) {
+      console.error((error.response as AxiosResponse<{ message: string }>)?.data.message);
+    }
   }
 })();
 
@@ -127,3 +184,11 @@ axiosB.isAxiosError = () => { };
 // 2. axios();
 // 3. axios.get();
 
+// get, post와 같은 메서드가 왜 Axios 클래스에 들어가 있을까?
+// AxiosStatic에 안들어가있고?
+// - 인스턴스 사용법 때문에 (new로 호출했을때도 사용하고 상속받은 쪽에서도 사용하기 위해서)
+// 예제
+const a = new axios.Axios({ url: 'localhost:8080' }).defaults;
+axios('localhost:8080');
+axios.get('localhost:8080');
+axios.defaults;
