3916404f3cfac56a084717b2c7e4e5c0bf152c25
leeyoonseo <okayoon.lee@gmail.com>
2022-09-06 15:07:53 +0900

    commonjs 모듈에 대해

---
+++ jquery.ts
@@ -0,0 +1,36 @@
+// 파일 내부에 모듈 관련된 내용이 있으면 파일 인식이 달라진다. (모듈시스템인지, 전역 스크립트인지)
+// import $ from 'jquery';
+// export {} 
+
+$('p').removeClass('myClass noClass').addClass('yourClass');
+$(['p', 't']).text('hello');
+
+const tag = $('ul li').addClass(function (index) {
+  return `item-${index}`;
+});
+
+$(tag).html(function (i: number) {
+  console.log(this);
+
+  return `${$(this).data('name')} 입니다`;
+});
+
+// Q. typescript가 commonjs로 module.exports 하는 법?
+// - javascript 일때
+// const $ = require('jquery);
+// module.exports = jQuery;
+// - typescript 일때
+// import $ = require('jquery);
+// export = jQuery;
+// - 모르겠을때???
+// import * as $ from 'jquery === import $ = require('jquery);
+
+// Q. 모듈시스템 흐름 (예시 react를 import할 때)
+// import React from 'react'; - 원칙적으로는 옳바르지않다
+// export = React; (=== module.exports = React)하고 있기 때문에 commonjs 문법 (export as namespace React가 있어서 엄밀히는 UMD)
+// 따라서 import React = require('react') (=== import * as React from 'react')해야한다.
+// 하지만 esModuleInterop: true로 설정하면, import React from 'react';가 가능하게 하는 것이다. (앞의 * as를 제거해도됨)
+
+// Q. 모든 라이브러리가 commonjs? -> No
+// export default -> esmodule
+// export = $ -> commonjs 
