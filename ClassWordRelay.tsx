bee7450e946992a4702ba4d1cef13c9a35f2b378
leeyoonseo <okayoon.lee@gmail.com>
2022-09-07 16:04:20 +0900

    react 타입분석 QnA까지 보기 완료

--- ClassWordRelay.tsx
+++ ClassWordRelay.tsx
@@ -1,4 +1,4 @@
-import React, { Component, FormEvent } from 'react';
+import React, { ChangeEvent, Component, createRef, FormEvent } from 'react';
 
 // props, state가 있을때
 interface Props {
@@ -11,15 +11,36 @@ interface State {
   result: string,
 }
 
+// Q. 함수 선언 방식에 따라서도 기능이 다르다.
+// 메서드 정의법이라고 생각할 수 있지만, 사소한 기능이 다르다. (이슈가 있다고 함) (=== 아직 ts가 완벽하지 않다를 설명하고 싶은듯)
+interface A {
+  a(): void;
+  b: () => void;
+}
+
 // <P = {}, S = {}, SS = any> -> 기본값이 있어서 안넣어도 에러가 나지 않지만, 타입 추론을 위해 넣으면 좋다.
 // interface Component<P = {}, S = {}, SS = any> extends ComponentLifecycle<P, S, SS> { }
 // class Component<P, S> {
+// Q. <P = {}, S = {}, SS = any> 에서 SS는 뭘까? -> snapshot???
+// componentDidUpdate?(prevProps: Readonly<P>, prevState: Readonly<S>, snapshot?: SS): void;
 class WordRelay extends Component<Props, State> {
   state = {
     word: '강아지',
     value: '',
     result: '',
   };
+
+  // Q. Ref 사용법
+  // type RefCallback<T> = { bivarianceHack(instance: T | null): void }["bivarianceHack"];
+  // - { bivarianceHack(instance: T | null): void }["bivarianceHack"]; 공변성, 반 공변성?과 연관있다고?
+  // - etc.ts 파일에 있지만, -> return 값은 넓은 타입으로 대입, 매개변수는 좁은 타입으로 대입 가능하다.
+  // - 결과는 type React.RefCallback<T> = (instance: T | null) => void -> bivarianceHack를 선언하고 뒤에서 바로 꺼내쓴 것
+  // type Ref<T> = RefCallback<T> | RefObject<T> | null;
+  // type LegacyRef<T> = string | Ref<T>;
+  input: HTMLInputElement | null = null;
+  onRefInput = (c: HTMLInputElement) => {
+    this.input = c;
+  }
 
   // interface FormEvent<T = Element> extends SyntheticEvent<T> {
   onSubmitForm = (e: FormEvent<HTMLFormElement>) => {
@@ -62,7 +83,7 @@ class WordRelay extends Component<Props, State> {
     }
   }
 
-  onChange = (e) => {
+  onChange = (e: ChangeEvent<HTMLInputElement>) => {
     this.setState({
       value: e.currentTarget.value,
     });
@@ -74,15 +95,16 @@ class WordRelay extends Component<Props, State> {
   // ReactNode는 ReactElement보다 더 확장 된 것
   render() {
     const { word, value, result } = this.state;
-    // const { inputEl, onsubmitForm, onChange } = this;
+    const { onRefInput, onSubmitForm, onChange } = this;
 
     // return 'hello';
 
     return (
       <>
         <div>{word}</div>
-        <form onSubmit={onsubmitForm}>
-          <input ref={inputEl}
+        <form onSubmit={onSubmitForm}>
+          <input
+            ref={onRefInput}
             value={value}
             onChange={onChange}
           />
@@ -108,3 +130,7 @@ export default WordRelay;
 // - namespace 방식도 겹치면 interface처럼 합쳐진다.
 // - 안겹치려면 import하는게 좋다.
 // - namespace는 script 태그로 불러올때 주로 사용한다.
+
+// Q. 다른 컴포넌트에 있는 el에 접근하려면? 
+// 클래스컴포넌트에서는 forwardRef를 사용하면된다.
+// 훅 컴포넌트에서는 useImperativeHandle를 사용하면된다.
