b548d43124ee5f1c528c84344bfdf67cb43e100e
leeyoonseo <okayoon.lee@gmail.com>
2022-09-07 14:06:22 +0900

    useState, useEffect 타이핑

--- WordRelay.tsx
+++ WordRelay.tsx
@@ -22,24 +22,49 @@ import React, { useState, useCallback, useRef, useEffect, FunctionComponent, FC 
 
 // props가 빈배열로 되어있으므로 제네릭 사용
 // interface FunctionComponent<P = {}> { 
-interface IWordRelay { name: string, title: string };
+// interface IWordRelay { name: string, title: string };
 // const WordRelay: FunctionComponent<IWordRelay> = (props) => {
 // FunctionComponent === FC (줄임말이 있음)
 
 // react 17버전에서는 VFC가 있었음 -> 18에서 없어지고 FC로 통일됨
 // VFC와 FC의 차이는 children?: ReaactNode 지원의 차이, 이후 버전에서 children을 사용안하게되고, 그러면 똑같기때문에 VFC가 사라짐
 // (버전 올릴때 주의)
-const WordRelay: FC<IWordRelay> = (props) => {
+const WordRelay: FC = () => {
 // 그외 이렇게 선언도 가능함
 // function WordRelay(props: P) {
 
+  // const [word, setWord] = useState(() => {
+  //   lazy init (리렌더링 시 성능최적화를 위해, 복잡한 함수를 초기에 실행하여 기억하게 만들기 위해 아래와 같이 사용함)
+  //   return 복잡함함수();
+  // });
+  // function useState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>];
+  // const [word, setWord] = useState<'강아지'>('강아지'); // 제네릭을 통해 타입 좁히기 가능
   const [word, setWord] = useState('강아지');
   const [value, setValue] = useState('');
   const [result, setResult] = useState('');
   const inputEl = useRef(null);
 
+  // useEffect의 EffectCallback 타입의 return type이 void로 고정되어있어서 useEffect를 async로 사용할 수 없다. 
+  // type EffectCallback = () => (void | Destructor);
+  // - async는 return 값이 Promise여야함.
+  // useEffect(async () => { // (X)
   useEffect(() => {
+    // useEffect에서 async를 못쓰므로 아래와 같이 패턴이 강제됨
+    // 1.
+    // const func = async () => {
+    //   await axios.post();
+    // };
+
     console.log('useEffect');
+    // 1.
+    // func();
+
+    // 2. cleanup 함수 이용 (Destructor)
+    // type EffectCallback = () => (void | Destructor);
+    return () => {
+      console.log('useEffect cleanup');
+      // cleanup 함수에 return 값은 없어야한다.
+    }
   }, []);
 
   const onsubmitForm = useCallback((e) => {
