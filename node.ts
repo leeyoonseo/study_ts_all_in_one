89df59287e821529d71445b58d4ae57d246bb275
leeyoonseo <okayoon.lee@gmail.com>
2022-09-11 16:03:25 +0900

    http, fs, path 모듈 타입 확인

--- node.ts
+++ node.ts
@@ -11,10 +11,19 @@
 
 import fs from 'fs';
 import http from 'http';
+// Q. from 'node:path'와 from 'path' 차이? node 모듈은 node:를 붙이는 것을 권장
+// import fs from 'node:fs';
+// import http from 'node:http';
 import path from 'node:path';
 
 // 시작 명령어: npx ts-node node.ts
-http.createServer((req, res) => {
+
+// Q. createServer 타이핑?
+// function createServer<
+// Request extends typeof IncomingMessage = typeof IncomingMessage,
+// Response extends typeof ServerResponse = typeof ServerResponse,
+// >(requestListener?: RequestListener<Request, Response>): Server<Request, Response>;
+const server = http.createServer((req, res) => {
   fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
     res.writeHead(200);
     res.end(data);
@@ -22,3 +31,6 @@ http.createServer((req, res) => {
 }).listen(8080, () => {
   console.log('서버 시작');
 });
+
+exports = server;
+// module.exports = server;
